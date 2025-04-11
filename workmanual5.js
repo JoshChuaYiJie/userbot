91f0f05ed35b73814c8d7dd4ade5c1aca35dc468
JoshChuaYiJie <JoshChuaYiJie@gmail.com>
2025-04-11 22:28:59 +0800

    Update workmanual5.js

--- workmanual5.js
+++ workmanual5.js
@@ -112,7 +112,7 @@ async function humanLikeScroll(page) {
 async function startBrowser() {
   console.log('Starting browser...');
   const browser = await puppeteerExtra.launch({ 
-    headless: true, 
+    headless: false, 
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
